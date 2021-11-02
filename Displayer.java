f98789d836b18dfbe9230ebc732012fc16ae69c7
Koh Haruki <harukikoh@gmail.com>
2021-11-02 17:29:54 +0800

    Template complete

--- Displayer.java
+++ Displayer.java
@@ -1,3 +1,13 @@
 public class Displayer {
-    
+    // Empty constructor
+    public Displayer() {
+    }
+
+    public void DisplayCardThrown(Card[] cardsThrown) {
+
+    }
+
+    public void DisplayPointsTable(Player[] allPlayers) {
+
+    }
 }
