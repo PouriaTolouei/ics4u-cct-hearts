2191afd743d48c2552b6509aa2d4f6b0451d1ea5
Koh Haruki <harukikoh@gmail.com>
2021-11-04 21:09:18 +0800

    Added Java Doc as well as suggestions for a new method for Displayer class

--- Displayer.java
+++ Displayer.java
@@ -3,10 +3,24 @@ public class Displayer {
     public Displayer() {
     }
 
+    // REMOVE THIS COMMENT: SUGGESTION: I think we should add a simple Display method
+    //                      where it explains the rules and notations. 
+    //                      (It's better to put as much Display into this method)
+
+    /* Displays the hand (the cards they have) of a specified player.
+     * @param player    - A Player whose hand will be displayed. */
+    public void DisplayHand(Player player) {
+
+    }
+
+    /* Displays the thrown cards that make up a trick in the order it was thrown.
+     * @param cardsThrown - An array of Card objects which are thrown by Player each round. */
     public void DisplayCardThrown(Card[] cardsThrown) {
 
     }
 
+    /* Displays the points of all players in a formatted way.
+     * @param allPlayers - An array of all Player objects, whose points will be displayed. */
     public void DisplayPointsTable(Player[] allPlayers) {
 
     }
